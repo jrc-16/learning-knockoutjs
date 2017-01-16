1eacfde50529c8b470346dc774cf72b60caf7074
jason carney <jrcarney.16@gmail.com>
2017-01-16 21:32:59 +0000

    Click binding example

--- js/app.js
+++ js/app.js
@@ -3,23 +3,21 @@
  */
 
 /** 
- * Often you will want to combine/convert multiple observable values to make other values
- * Below is a simple example of combining 2 observablue values, by using ko.computed()
- * Computed properties: are observable and based on the value of other observable values
-
- * How does it work?
- * Things are synchronised thanks to automatic dependency tracking - in the example fullName depends on firstName etc
- * When changes are made to firstName and lastName, this is reflected in fullName
- * A ripple effect triggers in the object graph causing the minimal set of refreshes needed to bring your viewmodel and UI up to date
-
-*/
+ * Capitlise persons last name
+ */
 function AppViewModel() {
 	this.firstName = ko.observable("Bert");
 	this.lastName = ko.observable("Bertington");
 
 	this.fullName = ko.computed(function() {
 		return this.firstName() + " " + this.lastName();
-	}, this)
+	}, this);
+
+	this.capitalizeLastName = function() {
+		// Get current lastName value: Notice that, to read or write an observable's value, you call it as a function.
+		var currentVal = this.lastName(); 
+		this.lastName(currentVal.toUpperCase()); // Set lastName to uppercase
+	}
 }
 
 // Activates knockout.js
--- readme.md
+++ readme.md
@@ -1,7 +1,14 @@
-Example of using knockout computed properties
+Example of using a click binding
+```html
+<button data-bind="click: capitalizeLastName">Go caps</button>
+```
+
+Invoked when we click on the capitalizeLastName binding
 ```javascript
-	this.fullName = ko.computed(function() {
-    	return this.firstName() + " " + this.lastName();    
-}, this);
+	this.capitalizeLastName = function() {
+		// Get current lastName value: Notice that, to read or write an observable's value, you call it as a function.
+		var currentVal = this.lastName(); 
+		this.lastName(currentVal.toUpperCase()); // Set lastName to uppercase
+	}
 ```
 
