badcedbeff1aad7e1f95a324bdf09ffdf4cbde7c
jason carney <jrcarney.16@gmail.com>
2017-01-20 21:30:39 +0000

    Added comments describing intent and logic

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 /**
- * @TODO: add description of what the below code is/does
+ * Loop through a list of people and display a meal 
  */
 
-/**
- * Class to represent a row in the seat reservations grid
- * Notice we use upper case 'S' to denote a Constructor function
- */
+// Class to represent a row in the seat reservations grid
+// Notice we use upper case 'S' to denote a Constructor function
+//
 function SeatReservation(name, initalMeal) {
 	var self = this; // Set 'this' to this functions scope
 	self.name = name;
@@ -17,13 +16,15 @@ function ReservationsViewModel() {
 	var self = this;
 
 	// Non editable catalog data - would come from the server
+	// but we hard code here so we have some dummy data
 	self.availableMeals = [
 		{ mealName: "Standard (sandwich)", price: 0 },
 		{ mealName: "Premium (lobster)", price: 34.95 },
 		{ mealName: "Ultimate (whole zebra)", price: 290 }
 	];
 
-	// Editable data
+	// seats is an observable array which contains reseravation
+	// objects
 	self.seats = ko.observableArray([
 		new SeatReservation("Steve", self.availableMeals[0]),
 		new SeatReservation("Bert", self.availableMeals[0]),
--- readme.md
+++ readme.md
@@ -1,4 +1,27 @@
 Very often, you'll want to generate repeating blocks of UI elements, especially when displaying lists where the user can add and remove elements. Knockout lets you do that easily, using *observable arrays* and the **foreach** binding.
 
-@TODO: add code examples
+Example of using a foreach binding to iterate over the 'seats' observable array
+
+```html
+<table>
+	<tbody data-bind="*foreach: seats*">
+	    <tr>
+	        <td data-bind="text: name"></td>
+	        <td data-bind="text: meal().mealName"></td><!-- // Access the meal objects properties by inkoving (meal is a ko observable) -->
+	        <td data-bind="text: meal().price"></td>
+	    </tr>
+	</tbody>
+
+    <!-- Todo: Generate table body -->
+</table>	
+
+Example of using an observable array. 'seats' it used by the foreach example above
+
+```javascript
+// seats is an observable array which contains reseravation
+// objects
+self.seats = ko.observableArray([
+	new SeatReservation("Steve", self.availableMeals[0]),
+	new SeatReservation("Bert", self.availableMeals[0]),
+]);
 
